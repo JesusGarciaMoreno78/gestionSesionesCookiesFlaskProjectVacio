ed72167148649220efe19e1e4e397c67b1a463bc
jesus <3607234@alu.murciaeduca.es>
2022-03-13 19:29:57 +0100

    Initial commit

---
+++ app/sesiones/routes.py
@@ -0,0 +1,100 @@
+import base64
+
+from flask import render_template, request, redirect, url_for, make_response, session
+from flask_login import login_user, logout_user, current_user
+
+import app
+from . import sesiones
+from .forms import LoginForm,UsuarioForm
+from .models import Usuario
+
+@app.login_manager.user_loader
+def load_user(user_id):
+    return Usuario.get_by_id(user_id)
+
+@sesiones.route("/logoutsession/")
+def logoutsession():
+    logout_user()
+    return redirect(url_for('sesiones.index'))
+
+@sesiones.route("/registrar/", methods=["get","post"])
+def registrar():
+    error = ""
+    form = UsuarioForm(request.form)
+    if form.validate_on_submit():
+        try:
+            usuario = Usuario()
+            usuario.username = form.username.data
+            usuario.set_password(form.password.data)
+            usuario.dni = form.dni.data
+            usuario.nombre = form.nombre.data
+            usuario.apellidos = form.apellidos.data
+            usuario.create()
+            return redirect(url_for("sesiones.loginsession"))
+        except Exception as e:
+            return render_template("registrar.html", form=form, error=e.__str__())
+    return render_template("registrar.html", form=form, error=error)
+
+@sesiones.route("/loginsession/", methods=["GET","POST"])
+def loginsession():
+    if current_user.is_authenticated:
+        return redirect(url_for('sesiones.welcome'))
+    form = LoginForm(request.form)
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        usuario = Usuario.get_by_username(username)
+        if usuario and usuario.check_password(password):
+            login_user(usuario, form.recuerdame.data)
+            return redirect(url_for('sesiones.welcome'))
+    return render_template('loginsession.html', form=form)
+
+@sesiones.route("/mostrarsession/")
+def mostrarsession():
+    nombre = session["nombre"]
+    apellidos = session["apellidos"]
+    return render_template("mostrarsession.html",nombre=nombre,apellidos=apellidos)
+
+@sesiones.route("/createsession/")
+def createsession():
+    session['nombre']="Jesús"
+    session['apellidos'] = "García Moreno"
+    return render_template('createsession.html')
+
+@sesiones.route("/mostrarcookie/")
+def mostrarcookie():
+    if request.method == "POST":
+        response = make_response(render_template("mostrarcookie.html"))
+        response.delete_cookie("nombre")
+        response.delete_cookie("apellidos")
+        nombre = ""
+        apellidos = ""
+        return response
+    else:
+        nombre=request.cookies.get("nombre")
+        apellidos=request.cookies.get("apellidos")
+    return render_template("mostrarcookie.html",nombre=nombre,apellidos=apellidos)
+
+@sesiones.route("/createcookie/")
+def createcookie():
+    response = make_response(render_template('createcookie.html'))
+    if request.method == "POST":
+        response.set_cookie("nombre", request.form.get("nombre"), max_age=60)
+        response.set_cookie("apellidos", request.form.get("apellidos"), max_age=60)
+    return response
+
+@sesiones.route("/xssreflejado/", methods=["GET","POST"])
+def xssreflejado():
+    if request.method == "POST":
+        comentario = request.form.get("comentario")
+        return comentario
+    return render_template("xssreflejado.html")
+
+@sesiones.route('/welcome/')
+def welcome():  # put application's code here
+    return render_template('welcome.html')
+
+@sesiones.route('/')
+def index():  # put application's code here
+    return render_template('index.html')
+
